aa1bd31d8d82838ed6febbfafb57de95c6e2188f
unknown <33382781+YuKimura45z@users.noreply.github.com>
2020-02-10 16:30:52 +0900

    [update]

--- projects/lib/src/lib/core/services/accounts/account.service.ts
+++ projects/lib/src/lib/core/services/accounts/account.service.ts
@@ -3,14 +3,18 @@ import { IAccount } from './i-account';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { isNotNull } from '../../operators/is-not-null';
 import * as firebase from 'firebase/app';
+import { AngularFireFunctions } from '@angular/fire/functions';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService<Account extends IAccount> {
   static readonly path = 'accounts';
 
-  constructor(private firestore: AngularFirestore) {}
+  constructor(
+    private firestore: AngularFirestore,
+    private functions: AngularFireFunctions,
+  ) {}
 
   /**
    *
@@ -30,8 +34,8 @@ export class AccountService<Account extends IAccount> {
    */
   accounts$(userID: string) {
     return this.firestore
-      .collection<Account>(AccountService.path, ref =>
-        ref.where('user_ids', 'array-contains', userID)
+      .collection<Account>(AccountService.path, (ref) =>
+        ref.where('user_ids', 'array-contains', userID),
       )
       .valueChanges({ idField: 'id' });
   }
@@ -42,14 +46,14 @@ export class AccountService<Account extends IAccount> {
    */
   accountsOfIDs(accountIDs: string[]) {
     return Promise.all(
-      accountIDs.map(accountID =>
+      accountIDs.map((accountID) =>
         this.firestore
           .collection<Account>(AccountService.path)
           .doc<Account>(accountID)
           .get()
           .toPromise()
-          .then(snapshot => snapshot.data() as Account)
-      )
+          .then((snapshot) => snapshot.data() as Account),
+      ),
     );
   }
 
@@ -65,7 +69,7 @@ export class AccountService<Account extends IAccount> {
     transaction: firebase.firestore.Transaction,
     userID: string,
     imageURL: string,
-    accountFactory: (iAccount: IAccount) => Account
+    accountFactory: (iAccount: IAccount) => Account,
   ) {
     const now = firebase.firestore.FieldValue.serverTimestamp() as firebase.firestore.Timestamp;
     const accountID = this.firestore.createId();
@@ -76,13 +80,13 @@ export class AccountService<Account extends IAccount> {
       image_url: imageURL,
       created_at: now,
       updated_at: now,
-      selected_at: now
+      selected_at: now,
     };
     transaction.set(
       this.firestore
         .collection<Account>(AccountService.path)
         .doc<Account>(accountID).ref,
-      accountFactory(iAccount)
+      accountFactory(iAccount),
     );
 
     return accountID;
@@ -106,5 +110,42 @@ export class AccountService<Account extends IAccount> {
    */
   getImagePath(accountID: string) {
     return `${AccountService.path}/${accountID}/image.jpg`;
+  }
+
+  /**
+   *
+   * @param accountID
+   */
+  async getUsers(accountID: string) {
+    return await this.functions
+      .httpsCallable('account_get_users')({
+        account_id: accountID,
+      })
+      .toPromise<{
+        uid: string;
+        email?: string;
+        emailVerified: boolean;
+        displayName?: string;
+        phoneNumber?: string;
+        photoURL?: string;
+        disabled: boolean;
+        metadata: {
+          lastSignInTime: string;
+          creationTime: string;
+        };
+        providerData: {
+          uid: string;
+          displayName: string;
+          email: string;
+          phoneNumber: string;
+          photoURL: string;
+          providerId: string;
+        }[];
+        passwordHash?: string;
+        passwordSalt?: string;
+        customClaims?: Object;
+        tokensValidAfterTime?: string;
+        tenantId?: string | null;
+      }>();
   }
 }
